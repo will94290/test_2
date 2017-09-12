d68284c078a6e3e39b807433d442730b5e31bfe0
William Tessari <will94290@yahoo.fr>
2017-09-12 15:50:31 +0200

    pb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  
   def index
-    @users = User.includes(:profile)
+    if params[:name] != nil
+      @users = User.select { |user| user.profile != nil && user.profile.first_name == "William"}
+      if @users.save
+        redirect_to "/users"
+      else
+        render action :new
+      end
+    else
+      @users = User.includes(:profile)
+    end
   end
   
   # /users/:id
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  post 'users' => 'users#index'
   root 'pages#home'
   resources :users do
     resource :profile

47e05ce2e5c98a678028a1a84ba95e1329c53b93
William Tessari <will94290@yahoo.fr>
2017-09-12 10:56:24 +0200

    first

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,47 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  # GET to /users/:user_id/profile/home
+  def new
+    # Render blank profile details form
+    @profile = Profile.new
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile updated !"
+      redirect_to "/users/#{params[:user_id]}"
+    else
+      render action :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile  
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated !"
+      redirect_to "/users/#{params[:user_id]}"
+    else
+      render action :edit
+    end
+  end
+
+   private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :city, :contact_email, :description, :service_offer, :reference)
+  end
+
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to(root_url) unless @user == current_user
+  end
+end

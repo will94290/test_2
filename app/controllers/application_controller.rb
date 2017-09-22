e88d17b76bee0e610211f67bb4b01e20e8e7e1a3
William Tessari <will94290@yahoo.fr>
2017-09-22 17:30:44 +0200

    avancement trad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
   $filepath = 'file.json'
   
   $country = File.read($filepath)
   
   $tab_word = JSON.parse($country)
-
+  
   $langue = "fr"
+  
 end

ab7fbb8a06da1e75a82e5ceafbdf56df17041561
William Tessari <will94290@yahoo.fr>
2017-09-25 12:45:42 +0200

    trad presque end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,29 @@ class UsersController < ApplicationController
   
   def index
     if params[:name] != nil && params[:name].length > 1
-      @users = User.includes(:profile).select { |user| user.profile != nil && user.profile.first_name.upcase == params[:name].upcase}
+      if params[:name].split.first.length >= 1 && params[:name].split[1] == nil
+        @users = User.includes(:profile).select { |user| user.profile != nil && (user.profile.first_name.upcase == params[:name].split.first.upcase || user.profile.last_name.upcase == params[:name].split.first.upcase) }
+      end
     else
       @users = User.includes(:profile)
     end
-    if params[:second] != nil && params[:second].length > 1
-      @users = @users.select { |user| user.profile != nil && user.profile.last_name.upcase == params[:second].upcase}
+    
+    if params[:name] != nil && params[:name].length > 1
+      if params[:name].split.first.length >= 1 && params[:name].split[1] != nil && params[:name].split[1].length >= 1
+        @users = User.includes(:profile).select { |user| user.profile != nil && ((user.profile.first_name.upcase == params[:name].split.first.upcase && user.profile.last_name.upcase == params[:name].split[1].upcase) || (user.profile.first_name.upcase == params[:name].split.last.upcase && user.profile.first_name.upcase == params[:name].split[1].upcase)) }
+      end
     else
       @users = @users
     end
-     if params[:job] != nil && params[:job] != "Job title"
-      @users = @users.select { |user| user.profile != nil && user.profile.job_title.upcase == params[:job].upcase}
-    else
-      @users = @users
+    
+     if params[:job] != nil && (params[:job] == "Developer" || params[:job] == "Developpeur")
+       @users = @users.select { |user| user.profile != nil && (user.profile.job_title.upcase == "Developer".upcase || user.profile.job_title.upcase == "Developpeur".upcase) }
+     elsif params[:job] != nil && (params[:job] == "Investor" || params[:job] == "Investisseur")
+       @users = @users.select { |user| user.profile != nil && (user.profile.job_title.upcase == "Investor".upcase || user.profile.job_title.upcase == "Investisseur".upcase) }
+     elsif params[:job] != nil && (params[:job] == "Contractor" || params[:job] == "Entrepreneur")
+       @users = @users.select { |user| user.profile != nil && (user.profile.job_title.upcase == "Contractor".upcase || user.profile.job_title.upcase == "Entrepreneur".upcase) }
+     else
+       @users = @users
      end
   end
   

1fe9aac0705a82b95e70d457094192d6a4f31f56
William Tessari <will94290@yahoo.fr>
2017-09-26 10:36:54 +0200

    file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  $filepath = 'home/william/Will/projet_1/file.json'
+  $filepath = 'file.json'
   
   $country = File.read($filepath)
   
